f2d99544017834044a4f9694492e30a2db8a5adb
sicXnull <31908995+sicXnull@users.noreply.github.com>
2019-04-19 21:27:35 -0400

    Update posqstrings.cpp
    update port

--- src/qt/posqstrings.cpp
+++ src/qt/posqstrings.cpp
@@ -253,10 +253,10 @@ QT_TRANSLATE_NOOP("posq-core", ""
 QT_TRANSLATE_NOOP("posq-core", ""
 "You must specify a masternodeprivkey in the configuration. Please see "
 "documentation for help."),
-QT_TRANSLATE_NOOP("posq-core", "(51472 could be used only on mainnet)"),
+QT_TRANSLATE_NOOP("posq-core", "(5510 could be used only on mainnet)"),
 QT_TRANSLATE_NOOP("posq-core", "(default: %s)"),
 QT_TRANSLATE_NOOP("posq-core", "(default: 1)"),
-QT_TRANSLATE_NOOP("posq-core", "(must be 51472 for mainnet)"),
+QT_TRANSLATE_NOOP("posq-core", "(must be 5510 for mainnet)"),
 QT_TRANSLATE_NOOP("posq-core", "<category> can be:"),
 QT_TRANSLATE_NOOP("posq-core", "Accept command line and JSON-RPC commands"),
 QT_TRANSLATE_NOOP("posq-core", "Accept connections from outside (default: 1 if no -proxy or -connect)"),
